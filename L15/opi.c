7e8aa97a549bc42c4ab38c4e5d4b95b9adc9b918
akshay97 <akshay97@vt.edu>
2018-03-13 17:08:26 -0400

    New commits

--- L15/opi.c
+++ L15/opi.c
@@ -1,29 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <omp.h>
 
 int main(int argc, char **argv) {
-
+  double start = omp_get_wtime();
+  double end; 
   //seed random number generator
   // Q2b: get the number of threads to run with from agrv and 
   // add OpenMP API code to set number of threads here
-  int Nthreads = 1;
+  int Nthreads = atoi (argv[0]);
+
+  omp_set_num_threads(Nthreads);
   
   struct drand48_data *drandData; 
   drandData = (struct drand48_data*) malloc(Nthreads*sizeof(struct drand48_data));
 
   // Q2c: add an OpenMP parallel region here, wherein each thread initializes 
   //      one entry in drandData using srand48_r and seed based on thread number
+   
   long int seed = 0;
+  #pragma omp parallel
+  {
   srand48_r(seed, drandData+0);
 
-  long long int Ntrials = 10000000;
+ // long long int Ntrials = 10000000;
 
 
   //need running tallies
+ // long long int Ntotal=0;
+ // long long int Ncircle=0;
+  }
+  //need running tallies
   long long int Ntotal=0;
   long long int Ncircle=0;
-
+  long long int Ntrials = 10000000;
+  #pragma omp parallel for reduction(+:Ncircle)
   for (long long int n=0; n<Ntrials; n++) {
     double rand1;
     double rand2;
@@ -43,6 +55,8 @@ int main(int argc, char **argv) {
       double pi = 4.0*Ncircle/ (double) (n);
       printf("Our estimate of pi is %g \n", pi);
     }
+    omp_get_wtime();
+    end = omp_get_wtime();
   }
 
   double pi = 4.0*Ncircle/ (double) (Ntotal);
